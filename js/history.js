107703045dcf0925e75a330e2b86261bd70e02d4
capnemo20 <z.sujon20@outlook.com>
2024-10-06 03:19:20 +0100

    buttons color change

--- js/history.js
+++ js/history.js
@@ -1,16 +1,26 @@
 document
   .getElementById("donation-history-btn")
   .addEventListener("click", function () {
-   document.getElementById("main-section").classList.add("hidden")
-   document.getElementById("main-section").classList.remove("active");
-   document.getElementById("history-section-details").classList.remove("hidden");
-   document.getElementById("history-section-details").classList.add("active");
+    document.getElementById("main-section").classList.add("hidden");
+    document.getElementById("main-section").classList.remove("active");
+    document
+      .getElementById("history-section-details")
+      .classList.remove("hidden");
+    document.getElementById("history-section-details").classList.add("active"); 
+    document.getElementById("donation-history-btn").style.backgroundColor =
+      "#008000"; 
+    document.getElementById("main-donation-btn").style.backgroundColor = "#DCDCDC"; 
   });
 document
   .getElementById("main-donation-btn")
   .addEventListener("click", function () {
-   document.getElementById("main-section").classList.add("active")
-   document.getElementById("main-section").classList.remove("hidden");
-   document.getElementById("history-section-details").classList.add("hidden");
-   document.getElementById("history-section-details").classList.remove("active");
+    document.getElementById("main-section").classList.add("active");
+    document.getElementById("main-section").classList.remove("hidden");
+    document.getElementById("history-section-details").classList.add("hidden");
+    document
+      .getElementById("history-section-details")
+      .classList.remove("active");
+      document.getElementById("main-donation-btn").style.backgroundColor =
+      "#008000"; 
+    document.getElementById("donation-history-btn").style.backgroundColor = "#DCDCDC"; 
   });
